2749f1a0dfe2e31d7b360ed944de7ab9f6a7d297
Pedro <pedrohalmeidamendonca@gmail.com>
2022-11-22 16:40:31 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import { act, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica o componente <NotFound.js />', () => {
+  test('Verifica se a página contém um heading "h2" com o texto "Page requested not found"', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/digimon');
+    });
+    const notFoundTitleEl = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(notFoundTitleEl).toBeInTheDocument();
+  });
+  test('Verifica se a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/digimon');
+    });
+    const notFoundImageEl = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(notFoundImageEl).toBeInTheDocument();
+    expect(notFoundImageEl).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
